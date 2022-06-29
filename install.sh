d6188f001cf6a7bd26a3aa9e80e4d9fac95dd769
etebo16 <108410648+etebo16@users.noreply.github.com>
2022-06-29 06:26:21 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear
+echo ""
+echo ""
+echo ""unzip session.zip
+unzip telethon.zip
+pip install -r requirements.txt
+sleep 10
+echo " create account on cashkaro.com "
+echo ""
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
